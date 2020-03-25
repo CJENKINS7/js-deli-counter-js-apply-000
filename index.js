6d48f4feeea7d21bb2954b148776a4c60b1c472c
congenial-principle-5422 <christinajenkins10@gmail.com>
2020-03-25 15:48:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function nowServing(katzDeliLine) {
 }
 
 function currentLine(katzDeliLine) {
-  if (katzDeliLine.length === 0) {
+  if (!katzDeliLine.length) {
     return "The line is currently empty."
   }
 // katzDeliLine = 0 means the amount of people should not go over 0
